4eb8409640af2b17124eb184adedf752d6c41e82
chrisjuin <c.juin.cj@gmail.com>
2017-09-27 15:30:26 +0200

    modif

---
+++ app.js
@@ -0,0 +1,17 @@
+$("#send").click(function(){
+
+$.ajax({
+    url : 'http://127.0.0.1/private/revision.php',
+    type: 'POST' ,
+    date : {name :'juin', surname:'chris' },
+    datatype : 'html' ,
+sucess : function(data){
+    $('#alert').html(data)
+},
+    error: function(){
+        $('#alert').html('')
+    },
+
+});
+
+});
